6a5fac244454eced4fafc73f7c893a747ea6a038
ralphcastillo2 <ralphcastillo2@gmail.com>
2025-05-15 23:12:21 -0700

    Fix TypeScript error in cities.ts

--- src/lib/cities.ts
+++ src/lib/cities.ts
@@ -3,13 +3,13 @@ import { City } from '@/types/City'
 import { WithId, Document } from 'mongodb'
 
 export async function getAllCities(): Promise<City[]> {
-  const { db } = await connectToDatabase()
+  const connection = await connectToDatabase()
   
-  if (!db) {
+  if (!connection || !connection.db) {
     throw new Error('Database connection failed')
   }
 
-  const cities = await db.collection('cities').find({}).toArray()
+  const cities = await connection.db.collection('cities').find({}).toArray()
   return cities.map(city => ({
     _id: city._id.toString(),
     name: city.name as string,
