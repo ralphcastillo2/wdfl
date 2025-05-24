3f59f1ab9ee2d051abbf7a439843437a623a5c35
ralphcastillo2 <ralphcastillo2@gmail.com>
2025-05-24 00:43:25 -0700

    modified: Dockerfile, next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'maps.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : undefined,
 };
 
 module.exports = nextConfig;
+
